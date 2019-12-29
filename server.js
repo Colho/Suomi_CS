e367a8c2bf831ba563012323f9af68472f4004d3
Colho <pentt.at@hotmail.com>
2019-12-29 20:36:44 +0200

    Team structure is saved to a file, and team structure update/creation is now its own function

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 const express = require('express');
 const request = require('request-promise');
+const fs = require('fs');
 //const { HLTV } = require('hltv')
 
 // Constants
@@ -10,40 +11,83 @@ const HOST = '0.0.0.0';
 const baseUrl = "https://api.pandascore.co";
 const token = "Pc6B5esWxcCJm0GBe9svCpHiTAmHJ0g6N6BzJlINmIW-jjic8jw";
 const teamSize = 50;
-var finTeams = {};
+var finTeams = [];
+/*
+EXAMPLE STRUCTURE
+[{
+  team_name: null,
+  team_id: null,
+  team_image_url: null,
+  players: [
+    {
+      name: null
+    }
+  ] 
+}]
+*/
 
 // App
 const app = express();
 app.get('/', async (req, res) => {
+  await updateTeams();
+  res.send("asd");
+});
+
+// Async function that can be used to update/create the team data in teams.txt
+async function updateTeams() {
   let keepGoing = true;
   let page = 1;
+  finTeams = [];
   while (keepGoing) {
+    // Get the paginated teams from PandaScore
     let response = await getTeams(page)
     var teams = JSON.parse(response);
+    // Go through the teams
     teams.forEach(function(team) {
-      //console.log(team.name);
+      // Go through the players in the team
       team.players.forEach(function(player) {
-        //console.log(player.last_name);
+        // If the player is from Finland, save the result
         if (player.hometown == "Finland") {
-          if (!finTeams.hasOwnProperty(team.name)) {
-            // Add team key here with info
+          // New team to be saved to JSON object
+          var index = teamAlreadySaved(team.name);
+          if (index == 0) {
+            var o = {};
+            o.team_name = team.name;
+            o.team_id = team.id;
+            o.team_image_url = team.image_url;
+            o.players = [];
+            var p = {};
+            p.name = player.name;
+            o.players.push(p);
+            finTeams.push(o);
           }
+          // Team already in the JSON object, add only the player
           else {
-            // Add relevant info under team key here
+            var p = {};
+            p.name = player.name;
+            finTeams[index].players.push(p);
           }
-          console.log(team.name + ":  " + player.name);
         }
       });
     });
     page += 1;
+    // If the package received is smaller than the default payload size, stop polling the service
     if (Object.keys(response).length < teamSize) {
       keepGoing = false;
-      //return records;
     }
   }
-  res.send("asd");
-});
+  //console.log(JSON.stringify(finTeams));
+  // Saving the team data to file for future use
+  fs.writeFile("teams.txt", JSON.stringify(finTeams), function(err) {
+    if(err) {
+      // Error handling?
+      return console.log(err);
+    }
+    //console.log("The file was saved!");
+  });
+}
 
+// Function to request teams
 const getTeams = async (page) => {
   const teamReq = {
       uri: `${baseUrl}/csgo/teams?sort=name&page=${page}`,
@@ -55,5 +99,18 @@ const getTeams = async (page) => {
   return payload;
 }
 
+// A function to check if a team is already saved in the struct.
+// Returns index if it is, returns 0 if not
+function teamAlreadySaved(teamName) {
+  var i;
+  for (i = 0; i < finTeams.length; i++) {
+      if (finTeams[i].team_name === teamName) {
+          return i;
+      }
+  }
+  return 0;
+}
+
+// Start server
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
