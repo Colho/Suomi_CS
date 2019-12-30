07dced3fcfa3ef0f60a797054e2cc7226c2bc66d
Colho <pentt.at@hotmail.com>
2019-12-30 23:09:59 +0200

    Added possible future routes, upcoming matches correctly fetched to a file. Possible to get running matches too.

--- server.js
+++ server.js
@@ -3,17 +3,18 @@
 const express = require('express');
 const request = require('request-promise');
 const fs = require('fs');
-//const { HLTV } = require('hltv')
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 const baseUrl = "https://api.pandascore.co";
 const token = "Pc6B5esWxcCJm0GBe9svCpHiTAmHJ0g6N6BzJlINmIW-jjic8jw";
-const teamSize = 50;
+const payloadSize = 50;
 var finTeams = [];
-/*
-EXAMPLE STRUCTURE
+var finMatches = [];
+/* Maybe initialize these?
+EXAMPLE STRUCTURES
+finTeams = 
 [{
   team_name: null,
   team_id: null,
@@ -24,14 +25,187 @@ EXAMPLE STRUCTURE
     }
   ] 
 }]
+finMatches = 
+[{
+  type: null (past, ongoing, upcoming),
+  start_time: null,
+  opponents: [
+    {
+      name: null,
+      team_image_url: null,
+      finPlayers: [
+        {
+          name: null
+        }
+      ]
+    }
+  ]
+  score_maps: null,
+  score_rounds: null,
+  league: null,
+  league_image_url: null
+}]
 */
 
-// App
+// Index
 const app = express();
 app.get('/', async (req, res) => {
-  await updateTeams();
+  //await updateTeams();
+  await updateMatches();
+  res.send(finMatches);
+});
+// Possible future routes for data fetching
+/*
+app.get('/getTeams', async (req, res) => {
+  //await updateTeams();
+  //await updateMatches();
   res.send("asd");
 });
+
+app.get('/getMatches', async (req, res) => {
+  //await updateTeams();
+  //await updateMatches();
+  res.send("asd");
+});
+
+app.get('/getPastMatches', async (req, res) => {
+  //await updateTeams();
+  //await updateMatches();
+  res.send("asd");
+});
+
+app.get('/getOngoingMatches', async (req, res) => {
+  //await updateTeams();
+  //await updateMatches();
+  res.send("asd");
+});
+
+app.get('/getUpcomingMatches', async (req, res) => {
+  //await updateTeams();
+  //await updateMatches();
+  res.send("asd");
+});
+
+app.get('/updateTeams', async (req, res) => {
+  //await updateTeams();
+  //await updateMatches();
+  res.send("asd");
+});
+
+app.get('/updateMatches', async (req, res) => {
+  //await updateTeams();
+  //await updateMatches();
+  res.send("asd");
+});
+
+app.get('/updatePastMatches', async (req, res) => {
+  //await updateTeams();
+  //await updateMatches();
+  res.send("asd");
+});
+
+app.get('/updateOngoingMatches', async (req, res) => {
+  //await updateTeams();
+  //await updateMatches();
+  res.send("asd");
+});
+
+app.get('/updateUpcomingMatches', async (req, res) => {
+  //await updateTeams();
+  //await updateMatches();
+  res.send("asd");
+});
+*/
+// Function that is used to create/update the matches data in past.txt, ongoing.txt and upcoming.txt
+async function updateMatches() {
+  // This needs checking, does this work correctly?
+  fs.readFile('teams.txt', (err, data) => {
+    if (err) throw err;
+    finTeams = JSON.parse(data);
+  });
+  // Past matches not yet done, because it needs some kind of time filter
+  //await paginateMatches('past');
+  await paginateMatches('running');
+  await paginateMatches('upcoming');
+}
+
+// A function to go through different types of matches. This needs some review
+// Is it really necessary to go through different kinds of matches separately.
+async function paginateMatches(type) {
+  let keepGoing = true;
+  let page = 1;
+  finMatches = [];
+  // Check is finTeams data correct?
+  
+  while (keepGoing) {
+    // Get the paginated matches from PandaScore
+    //console.log(type);
+    let response = await getMatches(page, type)
+    var matches = JSON.parse(response);
+    // Go through the matches
+    matches.forEach(function(match) {
+      // If no opponents, skip this match
+      if (match.opponents.length < 2) return;
+      //console.log(match.name);
+      //console.log(match.opponents[0].opponent.name);
+      //console.log(match.opponents[1].opponent.name);
+      // Go through the finnish teams
+      finTeams.forEach(function(team) {
+        // This should be more modular
+        if (match.opponents[0].opponent.name == team.team_name || match.opponents[1].opponent.name == team.team_name) {
+          //console.log(team.name);
+          var m = {};
+          m.type = type;
+          m.start_time = match.begin_at;
+          m.opponents = [];
+          var o1 = {};
+          var o2 = {};
+          o1.name = match.opponents[0].opponent.name;
+          o2.name = match.opponents[1].opponent.name;
+          o1.team_image_url = match.opponents[0].opponent.image_url;
+          o2.team_image_url = match.opponents[1].opponent.image_url;
+          // Add the finPlayers?
+          o1.finPlayers = [];
+          o2.finPlayers = [];
+          m.score_maps = null;
+          m.score_rounds = null;
+          m.league = null;
+          m.league_image_url = null;
+          m.opponents.push(o1, o2);
+          finMatches.push(m);
+        }
+      });
+    });
+    page += 1;
+    // If the package received is smaller than the default payload size, stop polling the service
+    if (Object.keys(response).length < payloadSize) {
+      keepGoing = false;
+    }
+  }
+  //console.log(JSON.stringify(finTeams));
+  // Saving the team data to file for future use
+  fs.writeFile(type + ".txt", JSON.stringify(finMatches), function(err) {
+    if(err) {
+      // Error handling?
+      return console.log(err);
+    }
+    //console.log("The file was saved!");
+  });
+}
+
+// Function to request matches
+// Needs 3 different cases, past, ongoing and upcoming
+// Past not yet done, because of the required time filter
+const getMatches = async (page, type) => {
+  const teamReq = {
+      uri: `${baseUrl}/csgo/matches/${type}?begin_at=name&page=${page}`,
+      headers : {
+        "Authorization" : "Bearer " + token
+      }
+  };
+  let payload = await request(teamReq);
+  return payload;
+}
 
 // Async function that can be used to update/create the team data in teams.txt
 async function updateTeams() {
@@ -72,7 +246,7 @@ async function updateTeams() {
     });
     page += 1;
     // If the package received is smaller than the default payload size, stop polling the service
-    if (Object.keys(response).length < teamSize) {
+    if (Object.keys(response).length < payloadSize) {
       keepGoing = false;
     }
   }
