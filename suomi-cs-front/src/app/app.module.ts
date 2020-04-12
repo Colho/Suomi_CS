68e3becb9c7a32775c1122cf10a2c4258d854716
Colho <pentt.at@hotmail.com>
2020-04-12 16:24:10 +0300

    Added basic Angular components for matches and teams

--- suomi-cs-front/src/app/app.module.ts
+++ suomi-cs-front/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatchComponent } from './match/match.component';
+import { TeamComponent } from './team/team.component';
+import { InfoComponent } from './info/info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatchComponent,
+    TeamComponent,
+    InfoComponent
   ],
   imports: [
     BrowserModule,
