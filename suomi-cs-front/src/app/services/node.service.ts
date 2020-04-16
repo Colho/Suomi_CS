0eec56eab0c526a8a4cd4f01c66db2c888fc9316
Atte Penttala <pentt.at@hotmail.com>
2020-04-16 21:20:16 +0300

    Moved node-service to service folder

--- suomi-cs-front/src/app/services/node.service.ts
+++ suomi-cs-front/src/app/services/node.service.ts
@@ -20,9 +20,17 @@ export interface Player {
 })
 export class NodeService {
 
+  teams : Array<Teams> = new Array();
+
   constructor(private http: HttpClient) { }
 
   fetchTeams(): Observable<Object> {
     return this.http.get<Array<Teams>>('/api/getTeams');
+  }
+
+  getTeams() {
+    this.http.get<Array<Teams>>('/api/getTeams').subscribe((data: Array<Teams>) => {
+      this.teams = { ...data };
+    });
   }
 }
